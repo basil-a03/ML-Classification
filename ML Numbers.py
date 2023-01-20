b4dfa48f8c87d0923006ea0ef91d594f57be1b82
basil180 <122956191+basil-a03@users.noreply.github.com>
2023-01-20 01:54:14 -0500

    Update ML Numbers.py

--- ML Numbers.py
+++ ML Numbers.py
@@ -1,74 +1,126 @@
-from six.moves import urllib
-opener = urllib.request.build_opener()
-opener.addheaders = [('User-agent', 'Mozilla/5.0')]
-urllib.request.install_opener(opener)
-
 import torch
+import matplotlib.pyplot as plt
+import helper
+from torchvision import datasets, transforms
 from torch import nn, optim
 import torch.nn.functional as F
-from torchvision import datasets, transforms
-import helper
 
-
-# Define transform to normalize the data
+# Define a transform to normalize the data
 transform = transforms.Compose([transforms.ToTensor(),
-                                transforms.Normalize((0.5,), (0.5,)),
-                              ])
+                                transforms.Normalize((0.5,), (0.5,))])
 # Download and load the training data
-trainset = datasets.MNIST('~/.pytorch/MNIST_data/', download=True, train=True, transform=transform)
+trainset = datasets.FashionMNIST('~/.pytorch/F_MNIST_data/', download=True, train=True, transform=transform)
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True)
 
-# NN
+# Download and load the test data
+testset = datasets.FashionMNIST('~/.pytorch/F_MNIST_data/', download=True, train=False, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=64, shuffle=True)
 
-model = nn.Sequential(nn.Linear(784, 128),
-                      nn.ReLU(),
-                      nn.Linear(128, 64),
-                      nn.ReLU(),
-                      nn.Linear(64, 10),
-                      nn.LogSoftmax(dim=1))
+## TODO: Define your model with dropout added
 
+class Classifier(nn.Module):
+    def __init__(self):
+        
+        super().__init__()
+        self.fc1 = nn.Linear(784, 377)
+        self.fc2 = nn.Linear(377, 153)
+        self.fc3 = nn.Linear(153, 75)
+        self.fc4 = nn.Linear(75, 10)
+        
+        # Dropout module with 0.2 drop probability
+        self.dropout = nn.Dropout(p=0.2)
+        
+    def forward(self, x):
+        # make sure input tensor is flattened
+        x = x.view(x.shape[0], -1)
+        
+        x = self.dropout(F.relu(self.fc1(x)))
+        x = self.dropout(F.relu(self.fc2(x)))
+        x = self.dropout(F.relu(self.fc3(x)))
+        x = F.log_softmax(self.fc4(x), dim = 1)
+        return x
+
+## TODO: Train your model with dropout, and monitor the training progress with the validation loss and accuracy
+
+model = Classifier()
 criterion = nn.NLLLoss()
-optimizer = optim.SGD(model.parameters(), lr=0.003)
+optimizer = optim.Adam(model.parameters(), lr=0.003)
 
+epochs = 30
+steps = 0
 
-epochs = 5
+train_losses, test_losses = [], []
 for e in range(epochs):
     running_loss = 0
     for images, labels in trainloader:
-        # Flatten MNIST images into a 784 long vector
-        images = images.view(images.shape[0], -1)
         
-        # Clearing gradients
         optimizer.zero_grad()
         
-        # Forward
-        output = model(images)
-        
-        #Calculating loss
-        loss = criterion(output, labels)
-        
-        # Gradient 
+        log_ps = model(images)
+        loss = criterion(log_ps, labels)
         loss.backward()
-        
-        
-        # Optimizing step, subtracting the gradient
         optimizer.step()
         
         running_loss += loss.item()
+        
+        
     else:
-        print(f"Training loss: {running_loss/len(trainloader)}")
+        tot_test_loss = 0
+        test_correct = 0
+        
+        # Validation pass and print out the validation accuracy
+        # turn off gradients
+        with torch.no_grad():
+            
+            # set model to evaluation mode
+            model.eval()
+            
+            # validation pass here
+            for images, labels in testloader:
+                # Forward
+                log_ps = model(images)
+                
+                # Estimate the number of loss
+                loss = criterion(log_ps, labels)
+                tot_test_loss += loss.item()
+                
+                # Estimate the number of correct
+                ps = torch.exp(log_ps)
+                top_p, top_class = ps.topk(1, dim=1)
+                equals = top_class == labels.view(*top_class.shape)
+                test_correct += equals.sum().item()
+        
+        
+        
+# Get mean loss to enable comparison between train and test sets
+train_loss = running_loss / len(trainloader.dataset)
+test_loss = tot_test_loss / len(testloader.dataset)
+# At completion of epoch
+train_losses.append(train_loss)
+test_losses.append(test_loss)
 
-# matplotlib inline
+# set model back to train mode
+model.train()
+
+print("Epoch: {}/{}.. ".format(e+1, epochs),
+        "Training Loss: {:.3f}.. ".format(train_loss),
+        "Test Loss: {:.3f}.. ".format(test_loss),
+        "Test Accuracy: {:.3f}".format(test_correct / len(testloader.dataset)))   # testloader.dataset = batch size             
 
 
-dataiter = iter(trainloader)
+model.eval()
+
+dataiter = iter(testloader)
 images, labels = next(dataiter)
+img = images[0]
+# Convert 2D image to 1D vector
+img = img.view(1, 784)
 
-img = images[0].view(1, 784)
-# Turn off gradients to speed up this part
+# Calculate the class probabilities (softmax) for img
 with torch.no_grad():
-    logps = model(img)
+    output = model.forward(img)
 
-# Output of the network are log-probabilities, need to take exponential for probabilities
-ps = torch.exp(logps)
-helper.view_classify(img.view(1, 28, 28), ps)
+ps = torch.exp(output)
+
+# Plot the image and probabilities
+helper.view_classify(img.view(1, 28, 28), ps, version='Fashion')
